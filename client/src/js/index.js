e3af39d188b4f09bf3247e6084619747aff5d816
Nishanth Shanmugham <nishanths@utexas.edu>
2016-03-16 06:27:38 -0500

    Prep client side

---
+++ client/src/js/index.js
@@ -0,0 +1,68 @@
+var spotify = require('./api/spotify');
+var request = require('request');
+var config = require('./config');
+var lodash = require('lodash');
+var algo = require('./closest');
+
+var songs = function(urls) {
+    promises = urls.map(function(url) {
+        return spotify.trackList(url);
+    });
+    return Promise.all(promises);
+};
+
+var trackURLs = function(playlists) {
+    return playlists.map(function(p) {
+        return p.tracks.href;
+    });
+};
+
+var tracks = function(songs) {
+    var tracks = [];
+    songs.forEach(function(obj) {
+        [].push.apply(tracks, obj.items.map(s => s.track));
+    });
+    return tracks;
+};
+
+var doAlgorithm = (tracks, minutes) => {
+    var shuffled = lodash.shuffle(tracks).slice(0, 400);
+    var obj = algo.closestSum(shuffled.map(s => Math.floor(s.duration_ms/1000)), minutes*60);
+
+    if (!obj.possible) {
+        return {matches: null, delta: null, possible: obj.possible};
+    }
+
+    var idxs = obj.list;
+
+    if (idxs != null) {
+        var matches = idxs.map(function(idx) {
+            return shuffled[idx];
+        });
+        return {matches: matches, delta: obj.delta, possible: obj.possible};
+    }
+};
+
+var start = () => {
+    spotify.playlists()
+        .then(trackURLs)
+        .then(songs)
+        .then(tracks)
+        .then(tracks => doAlgorithm(tracks, 2))
+        .then(function(obj) {
+            if (!obj.possible) {
+                return Promise.reject("Not possible with margin and delta");
+            }
+            obj.matches.forEach(m => console.log(m.name, m.duration_ms/1000))
+            return obj.matches;
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+    };
+
+
+module.exports = function() {
+    console.log(window.location);
+    spotify.refreshToken(start);
+};
---
+++ client/src/js/login.js
@@ -0,0 +1,14 @@
+var request = require('request');
+
+module.exports = function() {
+    // Listen for click events and simply set URL instead
+    // of making a GET request.
+    document.addEventListener('DOMContentLoaded', function() {
+        var spotifyButton = document.querySelector('#spotify-login');
+        if (!spotifyButton) return;
+
+        spotifyButton.addEventListener('click', function(e) {
+            window.location.href = "https://accounts.spotify.com/authorize/?client_id=8ae7f61cce1a449b913fbe16f2898a10&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8080&scope=user-read-private%20user-read-email%20playlist-read-private%20playlist-read-collaborative%20playlist-modify-public%20playlist-modify-private%20streaming%20";
+        });
+    });
+};
